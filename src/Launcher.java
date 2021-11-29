3ac5848d9df46bfd22be61422e6ec0eaf174d0ea
GaZotann <gazotan.minecraft@gmail.com>
2021-11-29 15:43:15 +0100

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,9 +7,18 @@ public class Launcher {
         Scanner KeybordInput = new Scanner(System.in);
 
         String command = KeybordInput.nextLine();
-        while (!command.equals("quit")){
+        while (!command.equals("quit") && !command.equals("fibo")){
             System.out.println("Unknow command");
             command = KeybordInput.nextLine();
+        }
+        if(command.equals("fibo")){
+            System.out.println("Tapez un nombre:");
+            int number = KeybordInput.nextInt();
+            int fibo = 0;
+            for(int i=0; i<=number; i++){
+                fibo += i;
+            }
+            System.out.println("voici la suite de Fibonacci est: " + fibo);
         }
     }
 }
